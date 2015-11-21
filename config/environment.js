8e0a411cafeec77863f3f47b48bdaac8d4c6d733
dieze <dieze51@gmail.com>
2015-11-21 22:41:10 +0100

    Update baseURL from '/' to '/first_app'

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'first_app',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/first_app',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
