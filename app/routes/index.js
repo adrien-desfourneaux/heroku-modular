d1fda8c1e02421763ca7735ff08abbbbe647f208
dieze <dieze51@gmail.com>
2015-11-22 00:50:34 +0100

    Fix call to second_app api

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return Ember.$.ajax({
-      url: '/api/second_app'
+      url: '/second_app/api'
     });
   }
 });
