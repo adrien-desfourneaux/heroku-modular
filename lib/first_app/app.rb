078611b0efa23cb118d148d5e1ffbc09c57840eb
dieze <dieze51@gmail.com>
2015-11-22 01:20:09 +0100

    Use Logger middleware to puts PATH_INFO

--- lib/first_app/app.rb
+++ lib/first_app/app.rb
@@ -1,5 +1,12 @@
 module FirstApp
+  class Logger
+    def initialize(app); @app = app; end
+    def call(env); puts env['PATH_INFO'].inspect; @app.call(env); end
+  end
+
   App = Rack::Builder.new {
+    use Logger
+
     map "/api" do
       run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['first_app']] }
     end
