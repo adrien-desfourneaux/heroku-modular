fbda2f8963a0dab7d2d94ae1c2c368a150d01182
dieze <dieze51@gmail.com>
2015-11-22 01:06:48 +0100

    Fix 404 on /api

--- lib/second_app/app.rb
+++ lib/second_app/app.rb
@@ -1,10 +1,10 @@
 module SecondApp
   App = Rack::Builder.new {
-    use Rack::Static, urls: [''], root: __dir__ + '/../../node_modules/second_app/dist', index: 'index.html'
-
     map "/api" do
       run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['second_app']] }
     end
+
+    use Rack::Static, urls: [''], root: __dir__ + '/../../node_modules/second_app/dist', index: 'index.html'
 
     run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['OK']] }
   }
